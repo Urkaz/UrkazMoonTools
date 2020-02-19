675aca976011f1ae0dbd906c113b4d6ae1cfdf44
Urkaz <mrurkaz@gmail.com>
2020-02-20 00:24:00 +0100

    Jar signing

--- src/main/java/com/urkaz/moontools/MoonToolsMod.java
+++ src/main/java/com/urkaz/moontools/MoonToolsMod.java
@@ -8,16 +8,18 @@ import com.urkaz.moontools.handlers.ConfigurationHandler;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLFingerprintViolationEvent;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = MoonToolsMod.MODID, name = MoonToolsMod.NAME, version = MoonToolsMod.VERSION, acceptedMinecraftVersions = MoonToolsMod.MC_VERSION)
+@Mod(modid = MoonToolsMod.MODID, name = MoonToolsMod.NAME, version = MoonToolsMod.VERSION, certificateFingerprint = MoonToolsMod.MOD_FINGERPRINT, acceptedMinecraftVersions = MoonToolsMod.MC_VERSION)
 public class MoonToolsMod {
 	public static final String MODID = "urkazmoontools";
 	public static final String NAME = "Urkaz Moon Tools";
-	public static final String VERSION = "1.2";
+	public static final String VERSION = "@VERSION@";
 	public static final String MC_VERSION = "[1.12.2]";
+	public static final String MOD_FINGERPRINT = "@FINGERPRINT@";
 
 	public static final Logger LOGGER = LogManager.getLogger(MoonToolsMod.MODID);
 
@@ -35,4 +37,10 @@ public class MoonToolsMod {
 	public void postInit(FMLPostInitializationEvent event) {
 
 	}
+	
+	@EventHandler
+    public void onFingerprintViolation(FMLFingerprintViolationEvent event) {
+        
+		LOGGER.warn("Invalid fingerprint detected! The file " + event.getSource().getName() + " may have been tampered with. This version will NOT be supported by the author!");
+    }
 }

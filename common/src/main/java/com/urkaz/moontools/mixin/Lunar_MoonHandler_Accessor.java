9667151830e369230726cbc8019eca2e2f90ecf5
Urkaz <mrurkaz@gmail.com>
2023-10-21 02:06:19 +0200

    Add compatibility with "Lunar" mod

---
+++ common/src/main/java/com/urkaz/moontools/mixin/Lunar_MoonHandler_Accessor.java
@@ -0,0 +1,33 @@
+/*
+ * This file is part of "Urkaz Mod Tools".
+ * Copyright (C) 2023 Urkaz
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+package com.urkaz.moontools.mixin;
+
+import com.mrbysco.lunar.client.MoonHandler;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(MoonHandler.class)
+public interface Lunar_MoonHandler_Accessor {
+
+    @Accessor("moonColor")
+    static float[] getMoonColor() {
+        throw new AssertionError();
+    }
+}

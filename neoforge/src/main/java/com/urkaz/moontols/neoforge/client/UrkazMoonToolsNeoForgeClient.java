8f8732216e2006555265a4c0c0aa8ca0395f17a0
Urkaz <mrurkaz@gmail.com>
2024-07-12 19:56:03 +0200

    Updte and add NeoForge

---
+++ neoforge/src/main/java/com/urkaz/moontols/neoforge/client/UrkazMoonToolsNeoForgeClient.java
@@ -0,0 +1,49 @@
+/*
+ * This file is part of "AlphaSIN 55".
+ * Copyright (C) 2024 Urkaz - Fran Sánchez
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+package com.urkaz.moontols.neoforge.client;
+
+import com.urkaz.moontools.UMTConstants;
+import com.urkaz.moontools.client.MoonClockColorHandler;
+import com.urkaz.moontools.common.UMTRegistry;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.neoforge.client.event.RegisterColorHandlersEvent;
+import net.neoforged.neoforge.common.NeoForge;
+
+@Mod(value = UMTConstants.MOD_ID)
+@Mod.EventBusSubscriber(value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD, modid = UMTConstants.MOD_ID)
+public class UrkazMoonToolsNeoForgeClient {
+
+    public UrkazMoonToolsNeoForgeClient(IEventBus modBus) {
+        NeoForge.EVENT_BUS.addListener(this::onHandleColors);
+    }
+
+    @SubscribeEvent
+    public static  void clientSetup(final FMLClientSetupEvent event) {
+        //event.enqueueWork(UrkazMoonToolsClient::clientInit);
+    }
+
+    private void onHandleColors(final RegisterColorHandlersEvent.Item event) {
+        event.getItemColors().register(new MoonClockColorHandler(), UMTRegistry.ITEM_MOONCLOCK);
+    }
+}
